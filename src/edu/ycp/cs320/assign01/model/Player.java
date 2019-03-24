c36fe81893d3a451c2b02d6a6854ec41534e60d3
Jaden Marini <jmarini@ycp.edu>
2019-03-24 17:13:51 -0400

    red x's

---
+++ src/edu/ycp/cs320/assign01/model/Player.java
@@ -0,0 +1,30 @@
+package edu.ycp.cs320.assign01.model;
+
+import java.util.ArrayList;
+
+public class Player {
+	ArrayList<Item> inventory, equipment;
+	int playerHealth;
+	
+	public Player() {
+		playerHealth = 100;
+		inventory = new ArrayList<Item>();
+		equipment = new ArrayList<Item>();
+	}
+	
+	public int attack() {
+		return 10;
+	}
+	
+	public void changeHealth(int change) {
+		playerHealth += change;
+	}
+	
+	public void addItem(Item item) {
+		inventory.add(item);
+	}
+	
+	public boolean isDead() {
+		return (playerHealth <= 0);
+	}
+}
