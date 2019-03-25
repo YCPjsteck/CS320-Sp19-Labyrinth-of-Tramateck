4ad77d9df4fb3608fe879d32d51cc1a31378fdba
CGTraina <47256790+CGTraina@users.noreply.github.com>
2019-03-25 11:34:42 -0400

    Added test cases to gameModelTest
    I added a test case to gameModelTest that adds monsters to rooms in a dungeon and checks to see if they appear

--- junit/edu/ycp/cs320/assign01/model/GameModelTest.java
+++ junit/edu/ycp/cs320/assign01/model/GameModelTest.java
@@ -3,6 +3,8 @@ package edu.ycp.cs320.assign01.model;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
+import java.util.ArrayList;
+
 import org.junit.Before;
 import org.junit.Test;
 
@@ -29,5 +31,40 @@ public class GameModelTest {
 		// is alive.
 		Player play = game.getPlayer();
 		assertFalse(play.isDead());
+	}
+	
+	@Test
+	public void testPopulate() {
+		
+		int[][] map = {	{1, 2, 0},
+						{0, 3, 0},
+						{0, 4, 0}};
+		
+		game.getDungeon().setMap(map);
+		game.getDungeon().generateRooms();
+		game.getDungeon().setPlayer(1, 0);
+		
+		ArrayList<String> monsters = new ArrayList<String>();
+		
+		monsters.add("skeleton");
+		Room room = game.getDungeon().getRoom(1);
+		room.populate(monsters);
+		monsters.clear();
+		
+		monsters.add("ghoul");
+		monsters.add("zombie");
+		room = game.getDungeon().getRoom(2);
+		room.populate(monsters);
+		monsters.clear();
+		
+		monsters.add("witch");
+		room = game.getDungeon().getRoom(4);
+		room.populate(monsters);
+		monsters.clear();
+		
+		assertTrue(game.getDungeon().getRoom(1).getMonsters().contains("skeleton"));
+		assertTrue(game.getDungeon().getRoom(2).getMonsters().contains("ghoul"));
+		assertTrue(game.getDungeon().getRoom(2).getMonsters().contains("zombie"));
+		assertTrue(game.getDungeon().getRoom(4).getMonsters().contains("witch"));
 	}
 }
