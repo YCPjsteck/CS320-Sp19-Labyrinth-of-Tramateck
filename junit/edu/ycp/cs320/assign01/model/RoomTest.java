20de67bc6754ff33accdec991b6a7c9366f2b870
CGTraina <47256790+CGTraina@users.noreply.github.com>
2019-04-08 10:58:04 -0400

    Added PlayerTest and RoomTest
    Added test cases for player and room class

---
+++ junit/edu/ycp/cs320/assign01/model/RoomTest.java
@@ -0,0 +1,53 @@
+package edu.ycp.cs320.assign01.model;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import java.util.ArrayList;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class RoomTest {
+	
+	private Room room;
+	
+	@Before
+	public void setup() {
+		room = new Room();
+	}
+	
+	@Test
+	public void testPopulate() {
+		/**
+		 * checks to see if each monster is properly added and has a unique id
+		 */
+		
+		ArrayList<String> monsters = new ArrayList<String>();
+		monsters.add("rat");
+		monsters.add("squid");
+		monsters.add("rat");
+		
+		room.populate(monsters);
+		assertTrue(room.getMonster(1) != null);
+		
+	}
+	
+	@Test
+	public void testRoomComplete() {
+		
+		ArrayList<String> monsters = new ArrayList<String>();
+		monsters.add("ghost");
+		monsters.add("skeleton");
+		
+		room.populate(monsters);
+		assertFalse(room.roomComplete());
+		
+		room.monsterKilled("skeleton");
+		assertFalse(room.roomComplete());
+		
+		room.monsterKilled("ghost");
+		assertTrue(room.roomComplete());
+	}
+
+}
